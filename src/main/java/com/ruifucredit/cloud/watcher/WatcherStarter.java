6d1ed49c1874b68439e573989e7d7e9552c976e1
leung-kay <kayonway@gmail.com>
2018-04-09 22:10:48 +0800

    解决turbine.stream长时间loading的问题
    主要是在启动类上增加@EnableDiscoveryClient注解

--- src/main/java/com/ruifucredit/cloud/watcher/WatcherStarter.java
+++ src/main/java/com/ruifucredit/cloud/watcher/WatcherStarter.java
@@ -2,10 +2,14 @@ package com.ruifucredit.cloud.watcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
+@EnableDiscoveryClient
 public class WatcherStarter {
 	
 	public static void main(String[] args) throws Exception {
